ab168634e210820c9e4837588a7f04a22b260ef6
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-04-28 18:31:37 +0100

    Functional parser

--- src/parsers/baseline_parser.py
+++ src/parsers/baseline_parser.py
@@ -1,5 +1,10 @@
 import json
+import pandas as pd
 import argparse
+import logging
+import copy
+
+logger = logging.getLogger(__name__)
 
 def get_tissue_mappings(mapping_filename):
     """Return a dictionary that maps tissue names to anatomical systems and organs"""
@@ -8,6 +13,50 @@ def get_tissue_mappings(mapping_filename):
         mapping_dict = json.load(mappings_file)
         return mapping_dict['tissues']
 
+def initialise_expression_dict(mapping_dictionary):
+
+    # Extract anatomical systems
+    anatomical_systems = set()
+    for tissues, mappings in mapping_dictionary.items():
+        anatomical_systems.update(mappings['anatomical_systems'])
+
+    # Initialise scoring dictionary
+    expression_dict={}
+    for anatomical_system in anatomical_systems:
+        expression_dict[anatomical_system]="No"
+        expression_dict[anatomical_system + " list"]=[]
+
+    return expression_dict
+
+def parse_baseline(baseline_filename, tissue_mapping, output_filename):
+
+
+    baseline_df = pd.read_csv(baseline_filename, sep='\t', header=0, index_col=0)
+
+    # Check that column names in baseline file exist in mapping file
+    columns_to_drop = []
+    for column in baseline_df.columns:
+        if column not in tissue_mapping:
+            logger.warning("{} is not a supported tissue, skipping it".format(column))
+            columns_to_drop.append(column)
+
+    # Drop unmapped tissues
+    if columns_to_drop:
+        baseline_df.drop(columns_to_drop, axis=1, inplace=True)
+
+    empty_expression_dict = initialise_expression_dict(tissue_mapping)
+    expression_per_anatomical_systems_dict = {}
+    # Iterate all genes
+    for gene, expression in baseline_df.to_dict('index').items():
+        expression_per_anatomical_systems_dict[gene] = copy.deepcopy(empty_expression_dict)
+        for tissue in expression:
+            # Gene is considered expressed if > 6 tpm
+            if expression[tissue] > 6:
+                for anat_sys in tissue_mapping[tissue]['anatomical_systems']:
+                    expression_per_anatomical_systems_dict[gene][anat_sys] = "Yes"
+                    expression_per_anatomical_systems_dict[gene][anat_sys + " list"].append(tissue)
+    expression_per_anatomical_systems_df = pd.DataFrame.from_dict(expression_per_anatomical_systems_dict, orient='index', columns=empty_expression_dict.keys())
+    expression_per_anatomical_systems_df.to_csv(output_filename, sep='\t')
 
 def main():
 
@@ -36,7 +85,7 @@ def main():
 
     # Load tissue mappings
     tissue_mappings = get_tissue_mappings(mapping_file)
-
+    parse_baseline(input_file, tissue_mappings, output_file)
 
 if __name__ == '__main__':
     main()
