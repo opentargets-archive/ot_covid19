027369d8d209b1265c394bfd2a6384ffa2177dc7
Davvid Ochoa <dogcaesar@gmail.com>
2020-05-01 19:56:06 +0100

    Preliminary parser for drug evidence informing target

---
+++ src/parsers/target_druginfo_parser.py
@@ -0,0 +1,33 @@
+import argparse
+import pandas as pd 
+
+def main():
+    
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Parse information from OT drug evidence and aggregates info at the target level.')
+
+    parser.add_argument('-i', '--input', help='OT drug evidence table', required=True, type=str)
+    parser.add_argument('-o', '--output', help='Output file name.', required=True, type=str)
+
+    args = parser.parse_args()
+
+    # Get parameters:
+    input_file = args.input
+    output_file = args.output
+
+    df = pd.read_csv(input_file, \
+                sep = "\t", \
+                names = ["id", "disease_id", "drug_id", "phase", "moa", "drug_name"])
+
+    result = df \
+        .groupby('id').agg(
+            max_phase = ('phase', 'max'),
+            drugs_in_clinic = ('drug_id', 'nunique')
+        ) \
+        .reset_index()
+
+    result.to_csv(output_file, sep='\t', index=False)
+
+if __name__ == '__main__':
+    main()
