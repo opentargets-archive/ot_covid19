6181bf5004074f6b49a1d33c7dfc3d7f85810a79
Asier Gonzalez <gonzaleza@ebi.ac.uk>
2020-05-05 17:25:36 +0100

    Add option to output information for all targets

--- src/parsers/safety_parser.py
+++ src/parsers/safety_parser.py
@@ -66,9 +66,22 @@ class Safety():
                                                              'safety_info_source' : ["experimental_toxicity"]
                                                              }
 
+    def add_targets_without_safety_info(self, filename):
+        """Add targets without target safety information to table"""
+
+        # Load gene info file an iterate through every gene
+        with gzip.open(filename, 'rt') as gene_file:
+            for line in gene_file:
+                gene_info_dict = json.loads(line.strip())
+                if gene_info_dict['ensembl_id'] not in self.target_safety_info:
+                    self.target_safety_info[gene_info_dict['ensembl_id']] = { 'has_safety_risk' : False,
+                                                                              'name' : "N/A",
+                                                                              'safety_organs_systems_affected' : "N/A",
+                                                                              'safety_info_source' : "N/A"
+                                                                              }
 
 
-    def parse_safety(self, known_safety_file, experimental_toxicity_file , compressed_gene_file, output_filename):
+    def parse_safety(self, known_safety_file, experimental_toxicity_file , compressed_gene_file, output_filename, output_all):
 
         # Load gene name to Ensembl id mappings
         self.get_gene_name2ensembl_mappings(compressed_gene_file)
@@ -78,6 +91,11 @@ class Safety():
 
         # Extract needed information from experimental toxicity file
         self.build_json_experimental_toxicity(experimental_toxicity_file)
+
+        # Include all targets in output file if flag is set.
+        # This is useful to have a more readable output after integration
+        if output_all:
+            self.add_targets_without_safety_info(compressed_gene_file)
 
         # Write to tsv file
         safety_df = pd.DataFrame.from_dict(self.target_safety_info, orient='index', columns=['name', 'has_safety_risk', 'safety_info_source', 'safety_organs_systems_affected'])
@@ -103,6 +121,7 @@ def main():
     parser.add_argument('-o','--output',
                         help='Output file name',
                         type=str, default='target_safety.tsv')
+    parser.add_argument('-a', '--allTargets', help='Output all targets and not only the ones with safety information', action='store_true')
 
     args = parser.parse_args()
 
@@ -111,9 +130,10 @@ def main():
     experimental_toxicity_file = args.experimentalToxicityFile
     gene_file = args.geneFile
     output_file = args.output
+    output_all_targets = args.allTargets
 
     safety = Safety()
-    safety.parse_safety(known_safety_file, experimental_toxicity_file, gene_file, output_file)
+    safety.parse_safety(known_safety_file, experimental_toxicity_file, gene_file, output_file, output_all_targets)
 
 
 if __name__ == '__main__':
