299e20754985181eec053ce9c1947773238c7ec5
Davvid Ochoa <dogcaesar@gmail.com>
2020-05-18 12:16:05 +0100

    duplicated rows issue fix ammended

--- src/parsers/abundances_get.R
+++ src/parsers/abundances_get.R
@@ -33,7 +33,7 @@ out <- df %>%
     mutate(abundance_reg_on_covid = str_split(abundance_reg_on_covid, ";")) %>%
     group_by(id) %>%
     summarise(is_abundance_reg_on_covid = any(is_abundance_reg_on_covid),
-              abundance_reg_on_covid = paste(unique(unlist(abundance_reg_on_covid)), collapse = ";"))
+              abundance_reg_on_covid = paste(unique(unlist(abundance_reg_on_covid[abundance_reg_on_covid != ""])), collapse = ";"))
 
 ## printing output
 out %>%
